84c31d09ff1418668c864b4ac5fdfd99a0089c5e
Evan <37721396+Xuleos@users.noreply.github.com>
2020-03-25 15:29:17 -0400

    clean test

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -14,25 +14,6 @@ export = () => {
 	it("Simply works", () => {
 		const Topology = new Kiawe.GridTopology(new Vector3(10, 1, 10), new Vector3(5, 5, 5));
 
-		const placeholderModel1 = new Instance("Model");
-
-		const placeholderPart = new Instance("Part");
-		placeholderPart.Anchored = true;
-		placeholderPart.Size = new Vector3(5, 5, 5);
-		placeholderPart.Color = Color3.fromRGB(255, 0, 0);
-		placeholderPart.Parent = placeholderModel1;
-
-		placeholderModel1.PrimaryPart = placeholderPart;
-
-		const placeholderModel2 = new Instance("Model");
-		const placeholderPart2 = new Instance("Part");
-		placeholderPart2.Anchored = true;
-		placeholderPart2.Size = new Vector3(5, 5, 5);
-		placeholderPart2.Color = Color3.fromRGB(0, 255, 0);
-		placeholderPart2.Parent = placeholderModel2;
-
-		placeholderModel2.PrimaryPart = placeholderPart2;
-
 		const AdjacencyModel = new Kiawe.AdjacencyModel(Topology, [
 			{
 				probability: 3,
